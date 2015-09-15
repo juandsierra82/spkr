7eccffb3782b0f10b192c148740b2502aaf74e68
Fangting <fangtingprahl@gmail.com>
2015-09-15 17:34:50 -0500

    server - add logout routes in userRoutes.js

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -5,6 +5,7 @@ module.exports = function (app) {
   // app === userRouter injected from middlware.js
   app.post('/login', userController.login);
   app.post('/signup', userController.signup);
+  app.post('/logout', userController.logout);
   app.get('/signedin', userController.checkAuth)
   app.get('/:id', userController.serveData);
   
