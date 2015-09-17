926eb0745e78bda7092c1b66c6a2c3e5b6197288
Fangting <fangtingprahl@gmail.com>
2015-09-17 16:54:38 -0500

    client - add makeBulletChart func and add tips

--- client/app/services/servicesBullet.js
+++ client/app/services/servicesBullet.js
@@ -48,18 +48,64 @@
           (score < min) && (min = score);
           (score > max) && (max = score);
           total += score;
+          commCount ++;
         })
-        average = Math.round(total / commData.length);
+        average = Math.round(total / commCount);
         bullet[i] = {title: criteria[i], ranges:[min, max, 100], measures: [userAvg], markers: [average] };
       };
 
       return bullet;
     };
 
+    var makeBulletChart = function(data) {
+      console.log("makeBulletChart data", data)
+      var margin = {top: 5, right: 40, bottom: 20, left: 120};
+      var width = 800 - margin.left - margin.right;
+      var height = 50 - margin.top - margin.bottom;
 
+      var chart = d3.bullet()
+            .width(width)
+            .height(height);
+
+      var svg = d3.select("#commBulletChart").selectAll("svg")
+            .data(data).enter().append("svg")
+            .attr("class", "bullet")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("tranform", "translate(" + margin.left + "," + margin.top + ")")
+            .call(chart);
+
+      var tip = d3.tip()
+            .attr("class", "d3-tip")
+            .offset([-10, 0])
+            .html(function (d) {
+              return "<strong>You:</strong> <span>" + d.measures[0] + "; </span>" + "<strong> Avg:</strong> <span>" + d.markers[0] + "; </span>"+ "<strong> Min:</strong> <span>" + d.ranges[0] + "; </span>" + "<strong>  Max:</strong> <span>" + d.ranges[1] + "</span>"
+            });
+
+      svg.call(tip);
+      
+      svg.on("mouseover", tip.show)
+        .on("mouseout", tip.hide)
+
+      var title = svg.append("g")
+            .style("text-anchor", "end")
+            .attr("transform", "translate(-6," + height / 2 + ")");
+
+      title.append("text")
+        .attr("class", "title")
+        .text(function(d){ return d.title; });
+
+      title.append("text")
+        .attr("class", "subtitle")
+        .attr("dy", "1em")
+        .text(function(d) { return d.title; });
+
+    };
     
     return {
-      makeBulletData: makeBulletData
+      makeBulletData: makeBulletData,
+      makeBulletChart: makeBulletChart
     }
   };
 
