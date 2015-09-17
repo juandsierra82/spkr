e946176dd499857ea00975e4a66416d2af1bbaec
Aaron Wolfe <aar.wolfe@gmail.com>
2015-09-17 10:46:42 -0500

    [client] Created placeholder functions for testing purposes

--- client/app/search/searchController.js
+++ client/app/search/searchController.js
@@ -1,8 +1,20 @@
 angular.module('spkr.search', [])
   .controller('SearchController', function ($scope, $window, $location, Auth, Pres) {
-    $scope.public = [];
-    
+    $scope.public;
+    $scope.match;
+
     $scope.search = function(val) {
-      
-    }
+      //this function will be invoked with the str in the input field everytime it is dirtied
+      //scope.match will be updated with the results of this function
+      console.log('executing the search function');
+    },
+
+    $scope.getPublic = function() {
+      //needs to return an array of results based off of the public prop being true
+      //then set scope.public to that array of results
+      console.log('executing the getPublic function');
+    },
+
+    $scope.getPublic();
+
   })
