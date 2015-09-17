c165cc9fe6e775602940f2e3aaf2275e4203ce3c
Aaron Wolfe <aar.wolfe@gmail.com>
2015-09-16 19:05:22 -0500

    [client] Set up some skeleton files for new search feature

---
+++ client/app/search/searchController.js
@@ -0,0 +1,8 @@
+angular.module('spkr.search', [])
+  .controller('SearchController', function ($scope, $window, $location, Auth, Pres) {
+    $scope.public = [];
+    
+    $scope.search = function(val) {
+      
+    }
+  })
