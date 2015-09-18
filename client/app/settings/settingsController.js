1f3f69703b5123e4e1f13f644f80f664ecb3e4e1
juandsierra82 <juandsierra82@gmail.com>
2015-09-18 14:44:42 -0500

    [toggle on and off] setting up server response

--- client/app/settings/settingsController.js
+++ client/app/settings/settingsController.js
@@ -1,10 +1,18 @@
 angular.module('spkr.settings', [])
-	.controller('SettingsController', function ($scope, $window){
+	.controller('SettingsController', function ($scope, $window, Set){
+		//making the settings object
 		var userid = $window.localStorage.getItem('userid');
 		$scope.settings = {
 				shared: false,
 				id: userid
 		}	
 
+		$scope.subPub = function (settings){
+			console.log("this is the user object,", settings)
+			Set.updateUser(settings).then(
+				function (data, err){
+					if(err) console.log(err);
+				})		
+		}
 	
 	})
