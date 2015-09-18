119d66448fa1f78a204a428cbaecadd0981ac124
Fangting <fangtingprahl@gmail.com>
2015-09-18 00:05:20 -0500

    client - bugFix - loggedIn user wont see sign/up button in landing page

--- client/app/index/backgroundController.js
+++ client/app/index/backgroundController.js
@@ -1,5 +1,10 @@
 angular.module('spkr.background', [])
   .controller('BackgroundController', function ($scope, $rootScope, $route, $routeParams, $window, $location, Auth) {
+  $scope.loggedIn = false;
+  $scope.$watch(Auth.isAuth, function(authed){
+     $scope.loggedIn = authed;
+   }, true);
+
   $rootScope.withBackground = true;
   $rootScope.withBackgroundPaths = ['/signup', '/login', '/'];
   $rootScope.$on('$locationChangeStart', function(event, next, current){
