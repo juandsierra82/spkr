67a6e7123346359abcb23ced98064156b70c1625
juandsierra82 <juandsierra82@gmail.com>
2015-09-18 14:09:11 -0500

    [toggleswitch] does not like subPub function

--- client/app/settings/settingsController.js
+++ client/app/settings/settingsController.js
@@ -1,13 +1,10 @@
-// angular.module('spkr.settings', [])
-// 	.controller('SettingsController', function ($scope, $window){
-// 		var userid = $window.localStorage.getItem('userid');
-// 		$scope.settings = {
-// 				shared: false,
-// 				id: userid
-// 		}	
-// 		$scope.subPub = function (user){
-// 			console.log("this is the user object," user)
+angular.module('spkr.settings', [])
+	.controller('SettingsController', function ($scope, $window){
+		var userid = $window.localStorage.getItem('userid');
+		$scope.settings = {
+				shared: false,
+				id: userid
+		}	
 
-// 		}
 	
-// 	})
+	})
